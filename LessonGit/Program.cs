74b4eafa06a46e907496a15f2514cfe3bf68fa73
Rasul Rashidov <rasidovrasul99@mail.ru>
2024-09-21 14:05:57 +0400

    Project initally added

---
+++ LessonGit/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LessonGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github created");
+            Console.WriteLine("Github ");
+            Console.WriteLine("Github");
+        }
+    }
+}
